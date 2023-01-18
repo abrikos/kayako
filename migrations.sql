30fa52bb045fa1b6ade9383722a9b4a2e571c685
Artem Filippow <a.filippov@atech.ru>
2023-01-18 20:12:51 +0900

    migrations

--- migrations.sql
+++ migrations.sql
@@ -1,8 +1,4 @@
 use kayako;
-alter table swuseremails add column isAdmin INT not null;
-alter table swuseremails add column userpassword varchar(255) not null;
-alter table tokens add column access_token varchar(255) not null;
-alter table tokens add column refresh_token varchar(255) not null;
 ALTER TABLE `swuseremails` ADD `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `userpassword`;
 ALTER TABLE `swuseremails` ADD `blocked` BOOLEAN NOT NULL DEFAULT true AFTER `userpassword`;
 UPDATE `swuseremails` SET `blocked` = '0' WHERE `swuseremails`.`email` = 'admin@test.com';
